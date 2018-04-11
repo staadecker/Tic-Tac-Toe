68cf54da065704f13b22d26285f0677a7a438a22
SUPERETDUPER <machst@live.ca>
2018-04-10 21:31:21 -0400

    Started migrating to FXML

--- src/tictactoe/Main.java
+++ src/tictactoe/Main.java
@@ -1,15 +1,14 @@
 package tictactoe;
 
 import javafx.application.Application;
-import javafx.beans.binding.Bindings;
-import javafx.beans.binding.NumberBinding;
-import javafx.geometry.Insets;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.Priority;
-import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
 
 public class Main extends Application {
     public static void main(String[] args){
@@ -17,10 +16,15 @@ public class Main extends Application {
     }
 
     @Override
-    public void start(Stage primaryStage) {
+    public void start(Stage primaryStage) throws IOException {
         JeuxHumainContreHumain jeux = new JeuxHumainContreHumain();
 
-        primaryStage.setScene(new Scene(jeux.getPlateauDeJeu(), 600, 600));
+        Parent pointage = FXMLLoader.load(getClass().getResource("/pointage.fxml"));
+
+        VBox root = new VBox(pointage, jeux.getPlateauDeJeu());
+
+
+        primaryStage.setScene(new Scene(root, 600, 600));
 //        primaryStage.setMaximized(true);
         primaryStage.show();
     }
---
+++ src/tictactoe/gui/PointageController.java
@@ -0,0 +1,9 @@
+package tictactoe.gui;
+
+import javafx.fxml.FXML;
+import javafx.scene.text.Text;
+
+public class PointageController {
+    @FXML private Text scoreCercle;
+    @FXML private Text scoreCroix;
+}
