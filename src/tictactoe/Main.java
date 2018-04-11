dc9b8fb01d3d623e6b446b0bd6dcc84998ca9efa
Martin Staadecker <machstg@gmail.com>
2018-04-11 12:54:16 -0400

    Made grid pane grow and added padding+spacing

--- src/tictactoe/Main.java
+++ src/tictactoe/Main.java
@@ -2,13 +2,14 @@ package tictactoe;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-import java.net.URL;
 
 public class Main extends Application {
     public static void main(String[] args){
@@ -22,7 +23,9 @@ public class Main extends Application {
         Parent pointage = FXMLLoader.load(getClass().getResource("/pointage.fxml"));
 
         VBox root = new VBox(pointage, jeux.getPlateauDeJeu());
-
+        VBox.setVgrow(jeux.getPlateauDeJeu(), Priority.ALWAYS);
+        root.setPadding(new Insets(10));
+        root.setSpacing(10);
 
         primaryStage.setScene(new Scene(root, 600, 600));
 //        primaryStage.setMaximized(true);
